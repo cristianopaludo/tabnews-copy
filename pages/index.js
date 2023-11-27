00eadab37eeeb5c67bdb43a104c60bb67b0cde0c
Cristiano Paludo <63319578+cristianopaludo@users.noreply.github.com>
2023-11-27 17:11:32 +0000

    Updated text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Quer programar comigo e além de ser minha perceira, ser minha colega pra vida?</h1>
+    return <h1>Quer programar comigo e, além de ser minha perceira, ser minha colega pra vida toda?</h1>
 }
 
 export default Home;
