25cdab40c39dc513e7318abba90fb838ff5a7060
Cristiano Paludo <63319578+cristianopaludo@users.noreply.github.com>
2024-02-18 19:58:06 +0000

    added scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-copy
+
 Implementation of https://www.tabnews.com.br to the course https://curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Quer programar comigo e, além de ser minha parceira, ser minha colega pra vida toda?</h1>
+  return (
+    <h1>
+      Quer programar comigo e, além de ser minha parceira, ser minha colega pra
+      vida toda?
+    </h1>
+  );
 }
 
 export default Home;
